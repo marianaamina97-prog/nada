3876fda0640988ba7204317f5ed8e4f8016fed62
Mariana Amina <marianaamina97@gmail.com>
2025-09-20 18:57:22 +0300

    =lesson10.js

--- lesson 10.js
+++ lesson 10.js
@@ -12,3 +12,13 @@ for (let p = 1; p <=20; p+=2) {
 
 for (let q = 1; q <=10; q++) {
   if(q%2 !==0) console.log(q);}
+
+  let coding=["html","css","js","react"];
+  for (let r = 0; r < coding.length; r++) {
+    console.log(coding[r]);}
+
+    for (let s=0; s<=5; s++) {
+        console.log(s);
+        for (let t=0; t<=2; t++) {
+            console.log("inner loop:"+t);}
+        }
