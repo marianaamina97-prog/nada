f0daacef758dba7d4b03cca603b940ce4f212e1f
Mariana Amina <marianaamina97@gmail.com>
2025-09-20 17:21:55 +0300

    Initial commit

---
+++ lesson 10.js
@@ -0,0 +1,14 @@
+for (let m = 1; m <=10; m++) {
+    console.log("count:"+m);
+    
+}
+
+for (let n = 10; n >=1; n--) {
+    console.log("count:"+n);}
+
+
+for (let p = 1; p <=20; p+=2) {
+    console.log("count:"+p);}
+
+for (let q = 1; q <=10; q++) {
+  if(q%2 !==0) console.log(q);}
---
+++ operate.js
@@ -0,0 +1,22 @@
+let x=27 % 5;console.log(x);
+let y=27 / 5;console.log(y);
+
+let marks=95;
+if(marks>=90){
+    console.log("A");}
+else if(marks>=80){
+    console.log("B");}
+else if(marks>=70){
+    console.log("C");}
+else if(marks>=60){
+    console.log("D");}
+else if(marks>=50){
+    console.log("c");}
+else{
+    console.log("F");}
+
+    let age=24;
+if(age>=18){
+    console.log("Adult");}
+else{
+    console.log("Minor");}
